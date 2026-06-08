6e92b070a7f07b5f71c1d198daa7ae19393f90c0
OlaveV-bit <heifu303@gmail.com>
2026-06-08 19:15:39 +0800

    Add files via upload

--- scripts/zn-m2-oc/diy-part2.sh
+++ scripts/zn-m2-oc/diy-part2.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 #
-# 版权所有 (c) 2026 OlaveV-bit
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
 #
-# 文件名：diy-part2.sh
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 #优先安装 openclash
