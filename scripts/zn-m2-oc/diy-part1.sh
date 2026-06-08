e8bd62a83a3f0d4408f04605311e1d7b714ad473
OlaveV-bit <heifu303@gmail.com>
2026-06-08 19:18:25 +0800

    Add files via upload

--- scripts/zn-m2-oc/diy-part1.sh
+++ scripts/zn-m2-oc/diy-part1.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 #
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+# 版权所有 (c) 2026 OlaveV-bit
 #
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
-# https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
+# 文件名：diy-part1.sh
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
